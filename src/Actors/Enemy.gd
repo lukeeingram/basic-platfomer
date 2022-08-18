f1745fff1e1419f2ffb5fc4a2bfa3a718d140d46
Luke Ingram <lingram217@gmail.com>
2022-08-18 11:07:51 -0400

    Change level, camera, enemy, bumper This changes the level layout, enhances camera behavior, changes the enemy so that it will detect a stomp, and allows the bumper to bounce the player when placed in the level.

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -4,8 +4,16 @@ func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
 
+func _on_StompDetector_body_entered(body: Node) -> void:
+	var physics_body := body as PhysicsBody2D
+	if physics_body.global_position.y < get_node("StompDetector").global_position.y:
+		get_node("CollisionShape2D").set_deferred("disabled", true)
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	_velocity.y = gravity * delta
 	if is_on_wall():
 		_velocity.x *= -1.0 
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+
