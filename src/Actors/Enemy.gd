fbfb1510756c4f55991bd99a511fd3c7eea06568
Luke Ingram <lingram217@gmail.com>
2022-08-18 18:26:53 -0400

    Add add'l enemy, change enemy slope behavior

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -6,9 +6,12 @@ func _ready() -> void:
 	_velocity.x = -speed.x
 
 func _physics_process(delta: float) -> void:
+	var snap := Vector2.DOWN * 50.0
 	_velocity.y = gravity * delta
 	_velocity.x *= -1 if is_on_wall() else 1
-	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+	_velocity.y = move_and_slide_with_snap(_velocity, snap, FLOOR_NORMAL).y
+	
+
 
 func kill() -> void:
 	queue_free()
