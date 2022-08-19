84d0b1fd1c456d52b60492df2ba9898dc0c8f02e
Luke Ingram <lingram217@gmail.com>
2022-08-19 12:20:51 -0400

    Current working template

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -4,13 +4,12 @@ class_name Enemy
 func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
-
+	
 func _physics_process(delta: float) -> void:
-	var snap := Vector2.DOWN * 50.0
-	_velocity.y = gravity * delta
+	"""_velocity.y = gravity * delta"""
+	var snap := Vector2.DOWN * 55.0
 	_velocity.x *= -1 if is_on_wall() else 1
 	_velocity.y = move_and_slide_with_snap(_velocity, snap, FLOOR_NORMAL).y
 	
-
 func kill() -> void:
 	queue_free()
