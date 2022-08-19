46c38f94111af0b1c47d28d4c0ef087a636e6d91
Luke Ingram <lingram217@gmail.com>
2022-08-19 12:25:51 -0400

    Add play, quit buttons and delete duplicate scene

--- src/UserInterface/PlayButton.gd
+++ src/UserInterface/PlayButton.gd
@@ -7,4 +7,4 @@ func _on_button_up() -> void:
 	get_tree().change_scene(next_scene_path)
 
 func _get_configuration_warning() -> String:
-	return "next_scene_path must be set for the button to work"
+	return "next_scene_path must be set for the button to work" if next_scene_path == "" else ""
